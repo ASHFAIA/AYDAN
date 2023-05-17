fc5f6a470f688e00c31bbf15f119bb7d5dbd28ea
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2023-05-17 13:36:40 -0700

    banking soon?

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AYDAN
 Aydan is a discord bot created to run commands and moderate school discord servers
+hehe micheal jackson
--- src/commands/bankInit.js
+++ src/commands/bankInit.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -5,6 +6,18 @@ module.exports = {
         .setName('initbank')
         .setDescription('Initialize the banking system for the server.'),
     run: ({ interaction, client, handler }) =>{
+        let serverID = interaction.server.id;
+        let path = "__dirname/../../data/banks/servers/"+serverID;
         
+        if(fs.existsSync(path))
+        {
+            interaction.reply("This server already has a banking system!")
+        }
+        else{
+            fs.cpSync('__dirname/../../data/banks/servers/templateServer',path, (err) =>{
+                if(err) console.log(err);
+            })
+            interaction.reply("Bank created!");
+        }
     }
 }
