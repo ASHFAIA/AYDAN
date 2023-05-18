dca69a1fc14b1af5fe604e287c41ae98360038ad
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2023-05-17 19:37:01 -0700

    implementing moderation

--- src/events/messageCreate/moderation/checkMessages.js
+++ src/events/messageCreate/moderation/checkMessages.js
@@ -1,9 +1,9 @@
-
+const List = require('__dirname/../../../../database/bannedWords.json').words;
 
 module.exports = (msg, client, handler) => {
     if(msg.author.tag == client.user.tag) return;
-    if(msg.content)
+    if(msg.content.some(i => List.includes(i)));
     {
-
+        msg.delete
     }
 }
