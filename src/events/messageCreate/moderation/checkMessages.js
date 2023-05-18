f9c029e577743b7d46fcbb6c193de38815eda602
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2023-05-17 20:23:39 -0700

    uggg

--- src/events/messageCreate/moderation/checkMessages.js
+++ src/events/messageCreate/moderation/checkMessages.js
@@ -1,4 +1,4 @@
-const List = require('__dirname/../../../../database/bannedWords.json').words;
+const List = require("__dirname/../../../../database/bannedWords.json");
 
 module.exports = (msg, client, handler) => {
     if(msg.author.tag == client.user.tag) return;
