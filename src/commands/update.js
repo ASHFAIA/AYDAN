433ab38fbdb3507dcf20e6342a136d23481cdbd3
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2023-05-18 19:30:06 -0700

    Attempting to implement updating uggg

---
+++ src/commands/update.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const child_process = require('child_process');
+require('dotenv').config();
+const AllowedAccounts = process.env.gitAccounts;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('update')
+        .setDescription('Pulls the latest update from github'),
+    run: ({interaction, client, handler}) => {
+        let author = interaction.author.id;
+        let processPath = process.env.batPath;
+        if(AllowedAccounts.includes(author))
+        {
+            interaction.reply("Pulling updates!")
+            child_process.exec(processPath, function(error, stdout, stderr) {
+                console.log(stdout);
+            })
+        }
+        else{
+            interaction.reply("You do not have the proper permission to do this!")
+        }
+
+    }
+}
